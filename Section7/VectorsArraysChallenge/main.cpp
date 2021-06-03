aac5d9bc70d5c8c7f7c15ef3f05e06b084763400
frankregal <regal1616@gmail.com>
2021-06-03 11:00:55 -0500

    Updated vector1.at(0) to 5000

--- Section7/VectorsArraysChallenge/main.cpp
+++ Section7/VectorsArraysChallenge/main.cpp
@@ -30,7 +30,7 @@ int main() {
     cout << "\nVector_2d row 2 elements: " << vector_2d.at(1).at(0) << endl;
     cout << "Vector_2d row 2 elements: " << vector_2d.at(1).at(1) << endl;
     
-    vector1.at(0) = 1000;
+    vector1.at(0) = 5000;
     
     cout << "\nVector_2d row 1 elements: " << vector_2d.at(0).at(0) << endl;
     cout << "Vector_2d row 1 elements: " << vector_2d.at(0).at(1) << endl;
