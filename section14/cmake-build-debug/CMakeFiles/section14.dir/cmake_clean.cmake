348daed54039933f36f06b7abcdf0e1e46caa98c
frank-Regal <fregal@utexas.edu>
2021-07-05 17:31:09 -0500

    ubuntu commit

---
+++ section14/cmake-build-debug/CMakeFiles/section14.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/section14.dir/main.cpp.o"
+  "section14"
+  "section14.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/section14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
